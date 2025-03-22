9a263df9bd12824cfb3e724c401a386ef43ba6ae
Jose <josefbautista94@gmail.com>
2025-03-22 18:44:05 -0400

    completed part 1

--- script.js
+++ script.js
@@ -4,14 +4,28 @@ const adventurer = {
     name: "Robin",
     health: 10,
     inventory: ["sword", "potion", "artifact"]
-    }
+}
 //Loop through and log each inventory item.
-for(let i of adventurer.inventory){
+for (let i of adventurer.inventory) {
     console.log(i)
 }
 
 //Add a nested object for a companion (Leo) inside adventurer.
-
+adventurer.companion = {
+    name: "Leo",
+    type: "Cat",
+}
 //Nest another object inside Leo: a flea named Frank with a couple of belongings.
+adventurer.companion.companion = {
+    name: "Frank",
+    type: "flea",
+    inventory: ["Small Hat", "Sunglasses"]
+}
 
 //Add a method, roll(mod = 0), that simulates a 1–20 die roll with an optional modifier.
+adventurer.roll = function (mod = 0 ) {
+    const result = Math.floor(Math.random() * 20 ) + 1 + mod;
+    console.log( `${this.name} rolled a: ${result}`)
+}
+
+adventurer.roll()
